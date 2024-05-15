1c8906a0d9ad0ed226c46ff8789051850cc83b13
Vikash Kumar <inyourserve@gmail.com>
2024-05-15 13:47:46 +0530

    updated-main
    updated with address

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.api.routers import users, events
-from app.api.routers import transactions
+from app.api.routers import users, events, addresses, transactions
 
 app = FastAPI()
 
 app.include_router(users.router,tags=["Users"])
 app.include_router(events.router,tags=["Events"])
 app.include_router(transactions.router, tags=["Transactions"])
+app.include_router(addresses.router, tags=["Address"])
+
